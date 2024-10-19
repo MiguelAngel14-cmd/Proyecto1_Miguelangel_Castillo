260a8da29590dc8d4ee098757b01e7d1c3fef3de
MiguelAngel14-cmd <miguelec.1405@gmail.com>
2024-10-19 11:29:40 -0400

    Modificacion JS

--- js.js
+++ js.js
@@ -1,2 +1,10 @@
 
 console.log ("Hola Mundo");
+
+for(let cont=1 ; cont<=20; cont++){
+    if (cont%2 === 0){
+        console.log ("El numero " + cont + " es par");
+    }else{
+        console.log ("El numero " + cont + " es impar");
+    }
+}
